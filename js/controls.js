9f80d72d7d69a10c4bceb1e7204255031a848b71
José Gregorio Ferrer Reyes <jose@vertex-pc0001>
2017-04-17 19:01:24 -0400

    Cargar toda la tarea de Moviles

---
+++ js/controls.js
@@ -0,0 +1,58 @@
+var app=angular.module('questionfinder', ['ngRoute']);
+
+app.filter('searchforms', function(){
+	return function(data, searchterm){
+		if(!searchterm){
+			return data;
+		}
+		var coincidences = [];
+		searchterm=searchterm.toLowerCase();
+		angular.forEach(data,function(form){
+			if(form.title.toLowerCase().indexOf(searchterm) !== -1){
+				coincidences.push(form);
+			};
+		});
+		return coincidences;
+	};	
+});
+
+app.controller('formsloadController', function ($scope,$http){
+	$scope.pagetitle='Questionfinder-Start';
+	$http.get("https://qfapi.herokuapp.com/v1/forms")
+	.then(function(response){
+		dat = response.data;
+		angular.forEach(dat,function(i){
+			i.created_at = i.created_at.substring(0,10);
+		})
+		$scope.forms = dat;
+	},function(response){
+		alert("Error al cargar datos");
+	});
+})
+
+app.config(function($routeProvider){
+  	$routeProvider.
+  		when("/details/:id/",{
+			templateUrl: "pages/details.html",
+			controller: "detailsController"
+  		}).
+  		when("/formlist",{
+  			templateUrl: "pages/formlist.html"
+  		}).
+  		otherwise({
+  			redirectTo: "formlist"
+  		});
+  	});
+
+app.controller('detailsController', function($scope,$routeParams,$http){
+	$scope.pagetitle='Questionfinder-Details';
+	id=$routeParams.id;
+	$http.get("https://qfapi.herokuapp.com/v1/forms/"+id)
+	.then(function(response){
+		dat = response.data;
+		dat.created_at = dat.created_at.substring(0,10);
+		$scope.form = dat;
+	},function(response){
+		alert("No se pudo detallar el cuestionario con el id: "+id);
+	})
+})
